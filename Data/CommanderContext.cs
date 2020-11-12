13f00ef1cd80499c39a30c62ac4492d9de3007c2
unknown <alia.azarian@gmail.com>
2020-11-12 16:29:58 +0000

    connection string added

---
+++ Data/CommanderContext.cs
@@ -0,0 +1,16 @@
+using Commander.Models;                     // Command
+using Microsoft.EntityFrameworkCore;        // DbContext
+
+namespace Commander.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+
+        }
+        // Represent the Command object down to our database as a DbSet called Commands
+        public DbSet<Command> Commands { get; set; }
+
+    }
+}
